bdf84bc4211314d6e0f03fc0183196d1ba802007
paulo <pauloferreira000@gmail.com>
2022-04-24 20:44:24 -0300

    Fix

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import numpy as np
-=======
->>>>>>> 138268d1165b80177657a9d3eff18b0485aa0fa7
 import cv2
 
 cap = cv2.VideoCapture(0)
@@ -40,9 +37,5 @@ while (True):
     if key == 27:
         break
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 138268d1165b80177657a9d3eff18b0485aa0fa7
 cap.release()
 cv2.destroyAllWindows()
